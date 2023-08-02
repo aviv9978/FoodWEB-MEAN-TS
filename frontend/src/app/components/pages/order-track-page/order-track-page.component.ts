3ac31ab588092b940707ca7374fd84d30d490282
aviv9978 <85827588+aviv9978@users.noreply.github.com>
2023-08-03 01:46:46 +0300

    finishing order truck page

--- frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
+++ frontend/src/app/components/pages/order-track-page/order-track-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/shared/models/Order';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-order-track-page',
   templateUrl: './order-track-page.component.html',
-  styleUrls: ['./order-track-page.component.css']
+  styleUrls: ['./order-track-page.component.css'],
 })
-export class OrderTrackPageComponent {
+export class OrderTrackPageComponent implements OnInit {
+  order$!: Observable<Order>;
 
+  constructor(
+    private orderService: OrderService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    const params = this.activatedRoute.snapshot.params;
+
+    if (!params.orderId) return;
+
+    this.order$ = this.orderService.trackOrderById(params.orderId);
+  }
 }
